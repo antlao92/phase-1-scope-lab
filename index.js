6a613eef3981f553e691754cf11148f8aafbfcdc
Anthony <anthonykimlao@gmail.com>
2023-12-13 14:23:02 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+var upperCaseCustomerName = function(){
+    customerName = "BOB"
+    return customerName;
+}
+
+setBestCustomer = function(){
+    return bestCustomer = "not bob";
+}
+ 
+overwriteBestCustomer = function(){
+    return bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = "tina";
+
+var changeLeastFavoriteCustomer = function (){
+    leastFavoriteCustomer= 30;
+    return leastFavoriteCustomer;
+}
